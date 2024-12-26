34d7489c699190a873e033b7c8f6e25a35a39189
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-12-26 14:12:57 -0800

    feat: add lenis and keyboard nav

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,10 @@
 import { animateHero } from "./animations/01-hero";
+import { lenis, gsap } from "./smooth-scrolling";
+import initKeyboardNavigation from "./keyboard-navigation";
 
-// Add event listeners or initialize animations
 window.addEventListener("load", () => {
   animateHero();
 });
+
+//Initialize Keyboard Navigation
+initKeyboardNavigation(lenis);
