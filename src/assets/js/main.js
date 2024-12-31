8b5b54fdb57eb99b5f875993ffc2041c849cdd64
ecstech-admin <james.strommer@ecstech.com>
2024-12-31 12:49:54 -0800

    feat: add sections 5, 6

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,6 +3,7 @@ import initKeyboardNavigation from './keyboard-navigation';
 import { initBulgeEffect } from './animations/01-hero';
 import { digitalHorizonsAnimations } from "./animations/02-digital-horizons";
 import { linesOfCodeAnimations } from "./animations/04-lines-of-code";
+import { engagementAnimations } from "./animations/06-engagement";
 import { animateHeadlinesStyle1 } from "./animations/headline-style-1";
 import { fadeInParagraphs } from "./animations/paragraph-fade-in";
 
@@ -13,6 +14,7 @@ initBulgeEffect('hero', '/assets/images/ohil-hero-bg.jpg');
 window.addEventListener("load", () => {
   digitalHorizonsAnimations();
   linesOfCodeAnimations();
+  engagementAnimations();
   animateHeadlinesStyle1(".headline-style-1");
   fadeInParagraphs(".p-animate"); 
 });
