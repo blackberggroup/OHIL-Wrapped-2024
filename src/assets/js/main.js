e03507e35ae0210bc120d7fa05b2b4c1a2fc2da1
RyanCrux <ryan.loper@thecruxfirm.com>
2025-01-02 13:21:57 -0500

    feat: blocking and copy for all sections

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,6 +3,7 @@ import initKeyboardNavigation from './keyboard-navigation';
 import { initBulgeEffect } from './animations/01-hero';
 import { digitalHorizonsAnimations } from "./animations/02-digital-horizons";
 import { linesOfCodeAnimations } from "./animations/04-lines-of-code";
+import { toolsAnimations } from "./animations/10-tools";
 import { codeAnimations } from "./animations/09-code";
 import { animateHeadlinesStyle1 } from "./animations/headline-style-1";
 import { fadeInParagraphs } from "./animations/paragraph-fade-in";
@@ -15,6 +16,7 @@ window.addEventListener("load", () => {
   digitalHorizonsAnimations();
   linesOfCodeAnimations();
   codeAnimations();
+  toolsAnimations();
   animateHeadlinesStyle1(".headline-style-1");
   fadeInParagraphs(".p-animate"); 
 });
