2ce5dbeec172e2867ede6891149578d43e534fb7
rb-blackberg <ross.burmeister@blackberggroup.com>
2025-01-13 20:49:12 -0800

    fix: test mobile fix

--- src/assets/js/smooth-scrolling.js
+++ src/assets/js/smooth-scrolling.js
@@ -4,18 +4,27 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const lenis = new Lenis({
-  smooth: true, 
-  lerp: 0.1, 
-});
+// const lenis = new Lenis({
+//   smooth: true, 
+//   lerp: 0.1, 
+// });
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
 
-requestAnimationFrame(raf);
+// requestAnimationFrame(raf);
 
-lenis.on("scroll", ScrollTrigger.update);
+// ScrollTrigger.scrollerProxy(document.body, {
+//   scrollTop(value) {
+//     return arguments.length ? lenis.scrollTo(value) : lenis.scroll;
+//   },
+//   getBoundingClientRect() {
+//     return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+//   },
+// });
+
+// lenis.on("scroll", ScrollTrigger.update);
 
 export { lenis, gsap };
